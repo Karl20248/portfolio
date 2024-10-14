d61f9d3b0d3e637f56b554ea9ca0194d0d0bfc1d
Кирилл Юдин <kyudin2001@mail.ru>
2024-10-14 13:03:52 +0300

    Работа со шкалой навыков

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const skills = {
+    data: [
+      { name: 'html', level: 80 },
+      { name: 'css', level: 80 },
+      { name: 'python', level: 60 },
+      { name: 'c++', level: 70 }
+    ],
+  
+    generateList(parentElement) {
+      this.data.forEach(skill => {
+        const skillItem = document.createElement('dt');
+        skillItem.classList.add('skill-item');
+  
+        switch (skill.name) {
+          case 'html':
+            skillItem.classList.add('skill-item_html');
+            skillItem.style.backgroundImage = 'url("../portfolio/img/skill-html.svg")';
+            break;
+          case 'css':
+            skillItem.classList.add('skill-item_css');
+            skillItem.style.backgroundImage = 'url("../portfolio/img/skill-css.svg")';
+            break;
+          case 'python':
+            skillItem.classList.add('skill-item_python');
+            skillItem.style.backgroundImage = 'url("../portfolio/img/skill-python.svg")';
+            break;
+          case 'c++':
+            skillItem.classList.add('skill-item_cpp');
+            skillItem.style.backgroundImage = 'url("../portfolio/img/skill-c++.svg")';
+            break;
+        }
+  
+        skillItem.textContent = skill.name;
+  
+        const skillLevel = document.createElement('dd');
+        skillLevel.classList.add('skill-level');
+  
+        const skillBar = document.createElement('div');
+        skillBar.style.width = `${skill.level}%`;
+        skillBar.textContent = `${skill.level}%`;
+  
+        skillLevel.append(skillBar);
+        parentElement.append(skillItem, skillLevel);
+      });
+    }
+  };
+  
+  const skillList = document.querySelector('dl.skill-list');
+  skills.generateList(skillList);
