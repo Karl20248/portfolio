9bee0b3356ee5937fec63b9e4188d28b70b5ac6c
Кирилл Юдин <kyudin2001@mail.ru>
2024-10-19 20:02:30 +0300

    Модифицировал код кнопок сортировки

--- js/script.js
+++ js/script.js
@@ -6,6 +6,11 @@ const skills = {
     { name: 'c++', level: 70 }
   ],
 
+  // Добавляем флаги для направления сортировки
+  sortByNameAsc: false,
+  sortByLevelAsc: false,
+  sortMode: null, // Добавляем новое свойство sortMode
+
   generateList(parentElement) {
     this.data.forEach(skill => {
       const skillItem = document.createElement('dt');
@@ -23,8 +28,69 @@ const skills = {
       skillLevel.append(skillBar);
       parentElement.append(skillItem, skillLevel);
     });
+  },
+
+  sortList(type) {
+    // Проверяем значение sortMode
+    if (this.sortMode === null || this.sortMode !== type) {
+      console.log(`отсортировали данные по ${type}`);
+    } else {
+      console.log('инвертировали порядок сортировки');
+    }
+
+    if (type === 'name') {
+      this.data.sort(this.compareByName);
+      // Меняем направление сортировки при повторном нажатии на кнопку
+      this.sortByNameAsc = !this.sortByNameAsc;
+      if (!this.sortByNameAsc) {
+        this.data.reverse();
+      }
+    } else if (type === 'level') {
+      this.data.sort(this.compareByLevel);
+      // Меняем направление сортировки при повторном нажатии на кнопку
+      this.sortByLevelAsc = !this.sortByLevelAsc;
+      if (!this.sortByLevelAsc) {
+        this.data.reverse();
+      }
+    }
+
+    // Устанавливаем значение sortMode равным типу сортировки
+    this.sortMode = type;
+
+    skillList.innerHTML = '';
+    this.generateList(skillList);
+  },
+
+  compareByName(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  },
+
+  compareByLevel(a, b) {
+    if (a.level < b.level) return 1;
+    if (a.level > b.level) return -1;
+    return 0;
   }
 };
 
 const skillList = document.querySelector('dl.skill-list');
+const sortBtnsBlock = document.querySelector('.skills-button');
+sortBtnsBlock.addEventListener('click', handleButtonClick);
+
+function handleButtonClick(event) {
+  if (event.target.nodeName === 'BUTTON') {
+    switch (event.target.dataset.type) {
+      case 'name':
+        skills.sortList('name');
+        break;
+      case 'level':
+        skills.sortList('level');
+        break;
+      default:
+        console.log('Неизвестная ошибка');
+    }
+  }
+}
+
 skills.generateList(skillList);
